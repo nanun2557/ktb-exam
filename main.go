8f305631bc091ea155c720f81602c957e2d0b427
nanun2557 <nanun2557@gmail.com>
2022-10-22 20:30:02 +0700

    update

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+)
+
+const defaultPort = "4000"
+
+func main() {
+	// Setup ----------------------------------------------------------
+	e := setupEcho()
+
+	// Start server ----------------------------------------------------------
+	go func() {
+		if err := e.Start(":" + defaultPort); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	// Use a buffered channel to avoid missing signals as recommended for signal.Notify
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+}
+
+func setupEcho() *echo.Echo {
+	e := echo.New()
+	e.Logger.SetLevel(log.INFO)
+	e.GET("/", func(c echo.Context) error {
+		time.Sleep(5 * time.Second)
+		return c.JSON(http.StatusOK, "OK")
+	})
+	return e
+}
